3543c1141b77651efe1cd3bf073403cb52025f75
AerialWombat <adrianzhan@gmail.com>
2019-03-17 17:56:17 -0400

    Created static query for optimized thumbnail images

--- src/components/Showcase/showcase.js
+++ src/components/Showcase/showcase.js
@@ -1,15 +1,38 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 
 import styles from "./showcase.module.scss"
 
-const Showcase = () => {
-  return (
-    <section className={styles.container}>
-      <article>
-        <h1 className={styles.header}>PROJECTS</h1>
-      </article>
-    </section>
-  )
-}
+const Showcase = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        thumbnailImgs: allFile(
+          filter: { relativeDirectory: { eq: "thumbnails" } }
+        ) {
+          edges {
+            node {
+              childImageSharp {
+                fluid(quality: 100, maxWidth: 1920) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const thumbnailDataEdges = data.thumbnailImgs.edges
+      return (
+        <section className={styles.container}>
+          <article>
+            <h1 className={styles.header}>PROJECTS</h1>
+          </article>
+        </section>
+      )
+    }}
+  />
+)
 
 export default Showcase
