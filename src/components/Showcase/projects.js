efec34647e3ca68f2b1815d7ca810788605554a8
AerialWombat <adrianzhan@gmail.com>
2019-03-19 18:14:04 -0400

    Set up projects data

---
+++ src/components/Showcase/projects.js
@@ -0,0 +1,30 @@
+const projects = [
+  {
+    name: "Formosa Cafe",
+    technologies: ["html5", "css3", "javascript", "react", "git", "github"],
+    description:
+      "A responsive, multi-page website built for a local restaurant that allows them to update menu items via Netlify CMS.",
+    imgPath: "formosa-cafe",
+    repoPath: "https://github.com/AerialWombat/restaurant-website",
+    demoPath: "https://www.formosacafefredericksburg.com/",
+  },
+  {
+    name: "weatherNow",
+    technologies: [
+      "html5",
+      "css3",
+      "sass",
+      "javascript",
+      "react",
+      "git",
+      "github",
+    ],
+    description:
+      "A responsive, single-page web application using the Dark Sky API and geocodio API. Get the current weather for any US city and a forecase for the next 3 days.",
+    imgPath: "weather-now",
+    repoPath: "https://github.com/AerialWombat/weather-now",
+    demoPath: "https://aerialwombat.github.io/weather-now/",
+  },
+]
+
+export default projects
