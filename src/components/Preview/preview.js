1de702546d429720a2d4787acc4dba81efe0aece
AerialWombat <adrianzhan@gmail.com>
2019-03-19 18:14:41 -0400

    Implemented showcase & project preview with styles

---
+++ src/components/Preview/preview.js
@@ -0,0 +1,72 @@
+import React from "react"
+import Img from "gatsby-image"
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaGit,
+  FaGithubSquare,
+  FaJsSquare,
+  FaReact,
+  FaSass,
+  FaNpm,
+} from "react-icons/fa"
+import { IconContext } from "react-icons"
+
+import styles from "./preview.module.scss"
+import { Link } from "gatsby"
+
+const icons = {
+  html5: FaHtml5,
+  css3: FaCss3Alt,
+  git: FaGit,
+  github: FaGithubSquare,
+  javascript: FaJsSquare,
+  react: FaReact,
+  sass: FaSass,
+  npm: FaNpm,
+}
+
+const Preview = ({
+  name,
+  fluidImg,
+  technologies,
+  description,
+  repoPath,
+  demoPath,
+}) => {
+  return (
+    <li className={styles.container}>
+      <div className={styles.projectImg}>
+        <Img fluid={fluidImg} alt="Screenshot of project" />
+        <ul className={styles.techList}>
+          {technologies.map(tech => {
+            const SpecificIcon = icons[tech]
+            return (
+              <IconContext.Provider value={{ size: "1.5em" }}>
+                <li>
+                  <SpecificIcon />
+                </li>
+              </IconContext.Provider>
+            )
+          })}
+        </ul>
+      </div>
+      <h1 className={styles.title}>{name}</h1>
+      <ul className={styles.projectDetails}>
+        <li>
+          <a href={repoPath} target="#" rel="noopener noreferrer">
+            Repo
+          </a>
+        </li>
+        <li>
+          <a href={demoPath} target="#" rel="noopener noreferrer">
+            Demo
+          </a>
+        </li>
+      </ul>
+      <p className={styles.description}>{description}</p>
+    </li>
+  )
+}
+
+export default Preview
--- src/components/Showcase/showcase.js
+++ src/components/Showcase/showcase.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
+import Preview from "../Preview/preview"
 
 import styles from "./showcase.module.scss"
+import "./projects"
+import projects from "./projects"
 
 const Showcase = () => (
   <StaticQuery
@@ -12,6 +15,7 @@ const Showcase = () => (
         ) {
           edges {
             node {
+              name
               childImageSharp {
                 fluid(quality: 100, maxWidth: 1920) {
                   ...GatsbyImageSharpFluid_withWebp
@@ -28,6 +32,24 @@ const Showcase = () => (
         <section className={styles.container}>
           <article>
             <h1 className={styles.header}>PROJECTS</h1>
+            <ul>
+              {thumbnailDataEdges.map(imgData => {
+                const project = projects.find(
+                  projectData => projectData.imgPath === imgData.node.name
+                )
+                return (
+                  <Preview
+                    key={project.imgPath}
+                    name={project.name}
+                    fluidImg={imgData.node.childImageSharp.fluid}
+                    technologies={project.technologies}
+                    description={project.description}
+                    repoPath={project.repoPath}
+                    demoPath={project.demoPath}
+                  />
+                )
+              })}
+            </ul>
           </article>
         </section>
       )
