d2269d0a5e26eaf124523a045cd9ee522fc63bae
AerialWombat <adrianzhan@gmail.com>
2019-03-20 19:54:51 -0400

    Added html lang attrivute

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,21 @@ import HeroBackground from "../components/HeroBackground/heroBackground"
 import About from "../components/About/about"
 import Showcase from "../components/Showcase/showcase"
 import Footer from "../components/Footer/footer"
+import favicon32 from "../../static/favicon32.png"
 
 import "./index.scss"
 
 export default () => (
   <div>
-    <Helmet>
+    <Helmet htmlAttributes={{ lang: "en" }}>
       <meta charSet="utf-8" />
       <title>Adrian Zhan</title>
       <meta
         name="description"
         content="Personal website for Adrian Zhan, web developer."
+        link={[
+          { rel: "shortcut icon", type: "image/png", href: `${favicon32}` },
+        ]}
       />
     </Helmet>
     <HeroBackground className="fixedBackground">
