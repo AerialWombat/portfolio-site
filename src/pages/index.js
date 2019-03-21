70bb6d946310922cc436f17867507f8b7d13cb7b
AerialWombat <adrianzhan@gmail.com>
2019-03-20 21:39:13 -0400

    Updated favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import HeroBackground from "../components/HeroBackground/heroBackground"
 import About from "../components/About/about"
 import Showcase from "../components/Showcase/showcase"
 import Footer from "../components/Footer/footer"
-import favicon32 from "../../static/favicon32.png"
 
 import "./index.scss"
 
@@ -14,13 +13,6 @@ export default () => (
     <Helmet htmlAttributes={{ lang: "en" }}>
       <meta charSet="utf-8" />
       <title>Adrian Zhan</title>
-      <meta
-        name="description"
-        content="Personal website for Adrian Zhan, web developer."
-        link={[
-          { rel: "shortcut icon", type: "image/png", href: `${favicon32}` },
-        ]}
-      />
     </Helmet>
     <HeroBackground className="fixedBackground">
       <Hero />
